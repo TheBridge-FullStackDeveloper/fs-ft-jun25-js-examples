2932264560ccf26696bf77b8575e163475ef3747
Alejandro Reyes <reyesbalejandroe@gmail.com>
2025-06-25 11:20:44 +0200

    add/fetch_async_await

--- demo_js_http_fetch/script.js
+++ demo_js_http_fetch/script.js
@@ -12,7 +12,7 @@
 //                       </article>`;
 //         });
 //         container += "</section>";
-  
+
 //         document.getElementById("resultado").innerHTML += container;
 //         return container;
 //       })
@@ -20,75 +20,65 @@
 //         console.error("Error fetching data from fakestoreapi:", error);
 //       });
 // }
-  
+
 //   // Llamada a la función
 // getAndRenderProducts().then((data) => console.log(data));
 // //console.log(getAndRenderProducts()); // esto no da tiempo a que se ejecute el return con los datos
 
 function getProducts() {
-    const url = "https://fakestoreapi.com/products?limit=10";
-    return fetch(url)
-      .then((response) => response.json()) // Convertir a objeto
-      .catch((error) => {
-        console.error("Error fetching data from fakestoreapi:", error);
-      });
+  const url = "https://fakestoreapi.com/products?limit=10";
+  return fetch(url)
+    .then((response) => response.json()) // Convertir a objeto
+    .catch((error) => {
+      console.error("Error fetching data from fakestoreapi:", error);
+    });
 }
-  
-  // Llamada a la función. Devuelve una promesa con los datos de la llamada a la API
-  getProducts().then((data) => console.log(data));
-  
-  // Llamada a la función. Devuelve una promesa con los datos de la llamada a la API
-  //Renderizar datos en el DOM
-  getProducts().then((data) => {
-    const ul = document.createElement("ul");
-    data.forEach((product) => {
-      //Tratamiento de datos y renderización en el DOM de cada item
-      const li = document.createElement("li");
-      li.textContent = product.title + " -- " + product.price + "€";
-      ul.appendChild(li);
-    });
-    // Vaciar el DOM primero
-    const oldContent = document.body.firstChild;
-    while (document.body.firstChild) {
-      document.body.removeChild(document.body.firstChild);
-    }
-    // Renderizar nuevos datos
-    document.body.appendChild(ul);
-  });
-  
-  // Llamada a la función. Devuelve una promesa con los datos de la llamada a la API
-  //Renderizar datos en el DOM. Utiliza template string para generar los nuevos elementos del DOM
-  
-  getProducts().then((data) => {
-    let list = "<ul>";
-    data.forEach((product) => {
-      list += `<li>${product.title} -- ${product.price}€</li>`;
-    });
-    list += "</ul>";
-  
-    document.body.innerHTML = list;
-  });
 
+// Llamada a la función. Devuelve una promesa con los datos de la llamada a la API
+getProducts().then((data) => console.log(data));
+
+// Llamada a la función. Devuelve una promesa con los datos de la llamada a la API
+//Renderizar datos en el DOM
+getProducts().then((data) => {
+  const ul = document.createElement("ul");
+  data.forEach((product) => {
+    //Tratamiento de datos y renderización en el DOM de cada item
+    const li = document.createElement("li");
+    li.textContent = product.title + " -- " + product.price + "€";
+    ul.appendChild(li);
+  });
+  // Vaciar el DOM primero
+  const oldContent = document.body.firstChild;
+  while (document.body.firstChild) {
+    document.body.removeChild(document.body.firstChild);
+  }
+  // Renderizar nuevos datos
+  document.body.appendChild(ul);
+});
+
+// Llamada a la función. Devuelve una promesa con los datos de la llamada a la API
+//Renderizar datos en el DOM. Utiliza template string para generar los nuevos elementos del DOM
+
+getProducts().then((data) => {
+  let list = "<ul>";
+  data.forEach((product) => {
+    list += `<li>${product.title} -- ${product.price}€</li>`;
+  });
+  list += "</ul>";
+
+  document.body.innerHTML = list;
+});
 
 //   Utiliza la API (https://dog.ceo/dog-api/) para resolver estos ejercicios.
 //  1.- Declara una funcion getAllBreeds que devuelva un array de strings con todas las razas de perro.
 
-function getAllBreeds(){
+function getAllBreeds() {
   return fetch("https://dog.ceo/api/breeds/list/all")
-  .then(res =>res.json())
-  .then(data=> Object.keys(data.message));
+    .then((res) => res.json())
+    .then((data) => Object.keys(data.message));
 }
 // console.log(getAllBreeds());
-getAllBreeds().then(breeds => console.log(breeds));
-
-
-
-
-
-
-/*******************/
-
-
+getAllBreeds().then((breeds) => console.log(breeds));
 
 /*********** Forma 1 -- LLamada a la API anidada ************/
 console.time("***timer1***");
@@ -109,3 +99,37 @@ fetch("https://rickandmortyapi.com/api/character/1") // Datos de Rick
         console.timeEnd("***timer1***");
       });
   });
+
+async function getEpisode3() {
+  try {
+    // Llamada 1
+    const res = await fetch("https://rickandmortyapi.com/api/character/1"); // Datos de Rick
+
+    // Verificar si la respuesta es exitosa
+    if (!res.ok) {
+      throw new Error(
+        `Error HTTP: ${res2.status} - ${res2.statusText}`
+      );
+    }
+
+    const rickMortyData = await res.json();
+
+    // URL tercer episodio donde aparece
+    const episode_url_3 = rickMortyData.episode[2];
+
+    // Llamada 2
+    const res2 = await fetch(episode_url_3); // Datos episodio 3
+
+    // Verificar si la respuesta es exitosa
+    if (!res2.ok) {
+      throw new Error(`Error HTTP: ${res2.status} - ${res2.statusText}`);
+    }
+    const episode_data = await res2.json();
+
+    return episode_data;
+  } catch (error) {
+    console.log(`ERROR: ${error.stack}`);
+  }
+}
+
+getEpisode3().then((episode_3) => console.log(episode_3));
